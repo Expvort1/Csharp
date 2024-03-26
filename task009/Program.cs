a6a8eced46b1151da8e743f5adfd0a98b5a09d0a
Expvort1 <twidroyd1525@gmail.com>
2024-03-26 10:04:46 +0300

    Случайно чиисло

---
+++ task009/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая выводлит случайное число из отрезко [10,99] и показывает наибольшую цифру числа.
+78 -> 3
+12 -> 2
+85 -> 8
+*/
+
+int number = new Random(). Next(10, 100);
+System.Console.WriteLine(number);
+
+int first = number / 10;
+int second = number % 10;
+int max = first;
+
+if (second > first)
+{
+    max = second;
+}
+System.Console.WriteLine(max);
