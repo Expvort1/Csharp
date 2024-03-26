05115723d6bc92eca5d8489eb43afe94f5610fb9
Expvort1 <twidroyd1525@gmail.com>
2024-03-26 10:13:49 +0300

    Убрать центральное число

---
+++ task011/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+Напишите задачу, которая будет выдавать случайное 3-ех значное число и удалять у него центральную цифру
+*/
+
+int number = new Random(). Next(100, 1000);
+System.Console.WriteLine(number);
+
+int dig1 = number / 100;
+int dig2 = number % 10;
+System.Console.WriteLine(dig1*10 + dig2);
