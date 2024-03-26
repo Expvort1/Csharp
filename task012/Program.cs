2d2701abf4a2f09e01fce4f11d866755cc6c1cd0
Expvort1 <twidroyd1525@gmail.com>
2024-03-26 11:07:10 +0300

    Принемает 2 числа выдает кратное

---
+++ task012/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая на вход будет принемать 2 числа и выводить, является ли число крастное воторому. 
+Если второе число некратно первому, то программа выводит остаток от деления.
+*/
+
+Console.WriteLine("Введите два числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % number2 ==0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("Некратно " +number1 % number2);
+}
