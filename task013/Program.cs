b5e1c7437cc46a0bc99fc92faca98da3d0012dfb
Expvort1 <twidroyd1525@gmail.com>
2024-03-26 11:27:17 +0300

    Кратное ли число?

---
+++ task013/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая принемает число на вход и проверяет, кратно ли оно 7 и 23 одновнеменно
+*/
+
+System.Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 ==0)
+{
+    System.Console.WriteLine("Кратное");
+}
+else
+{
+    System.Console.WriteLine("Некратно");
+}
