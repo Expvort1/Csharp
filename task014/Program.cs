f2754648bf952a02da90aa5e1a4de8a7c2c9d19a
Expvort1 <twidroyd1525@gmail.com>
2024-03-26 11:51:51 +0300

    Крастность числа

---
+++ task014/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу, которая проверят кратное ли число друг другу.
+*/
+
+System.Console.WriteLine("Введите два числа: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (Math.Pow(number, 2) == number2 || Math.Pow (number2, 2) == number)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
