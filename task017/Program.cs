63f25b4ee9b761c146d86f6ab3a77315c9271737
Expvort1 <twidroyd1525@gmail.com>
2024-04-02 11:44:24 +0300

    Add Task017 Проверка числа из массива

---
+++ task017/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задайте масив. Напишите программу, которая определяет,
+присутствует ли заданное число в массиве. Программа 
+должна выдавать ответ: Да/Нет.
+*/
+
+int[] array ={1, 3, 4, 19, 3};
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int v = 0;
+
+foreach (int e in array)
+{
+    if (e == x) {v = x;}
+}
+
+if (v == x)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
